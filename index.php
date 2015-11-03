6fd23b276a754abeb2269aae1c494c150d1412cd
Gabriel Borges <borgesbox@gmail.com>
2015-11-03 16:43:26 -0200

    LOL

--- index.php
+++ index.php
@@ -3,8 +3,26 @@ require 'vendor/autoload.php';
 
 $app = new \Slim\Slim();
 
+//Hello World 
 $app->get('/hello/:name', function ($name) {
-        echo "Hello, " . $name;
+        $inteiro1 = 42;
+        $inteiro2 = 79;
+        $texto = "ola, ";
+        $fracao = 2.3;
+        
+        $soma = $inteiro1 + $inteiro2;
+        $divisao = $inteiro1 / $fracao;
+        $multi = $inteiro2 * $fracao;
+        $subtracao = $inteiro1 - $inteiro2;
+        
+        $olaMundo = $texto . $name;
+        
+        echo "<p>Soma: " . $soma . "</p>";
+        echo "<p>Divisao: " . $divisao . "</p>";
+        echo "<p>Multiplica: " . $multi . "</p>";
+        echo "<p>Subtracao; " . $subtracao . "</p>";
+        echo "<p>" . $olaMundo . "</p>"; 
 });
 
 $app->run();
+
